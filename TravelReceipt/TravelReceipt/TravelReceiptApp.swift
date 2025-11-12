fc9795d033022c5b868f04b2504d7f380aa94d48
陳憶柔 <161418887+D1204387@users.noreply.github.com>
2025-11-12 22:51:14 +0800

    Initial commit for TravelReceipt iOS app
    Set up a new SwiftUI iOS project named TravelReceipt. Added project configuration files, asset catalogs, and basic ContentView and App entry point.

---
+++ TravelReceipt/TravelReceipt/TravelReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelReceiptApp.swift
+//  TravelReceipt
+//
+//  Created by YiJou  on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct TravelReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
